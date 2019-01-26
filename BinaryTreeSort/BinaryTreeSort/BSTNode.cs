6abdfe29400354c94bac771c3a24e81524390c20
Muhammad Ismail <46966245+ismailyaqoob@users.noreply.github.com>
2019-01-26 18:03:20 +0500

    Add files via upload

---
+++ BinaryTreeSort/BinaryTreeSort/BSTNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeSort
+{
+    //BSTNode class containing data with the reference of left node and right node.
+    class BSTNode
+    {
+        public int data;
+        public BSTNode leftnode, rightnode;
+        public BSTNode()
+        {
+            leftnode = null;
+            rightnode = null;
+        }
+    }
+}
